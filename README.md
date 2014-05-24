e84564e08c095f4b236a898232fa683033ab1996
Stevica Kuharski <kstevica@gmail.com>
2014-05-24 11:04:06 +0000

    PWM settings, LED light is as strong as movement is

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Raspberry Pi LEDccelerometer
 Turn on LEDs using Raspberry Pi with @pi_borg XLoBorg and TriBorg
 
 Video is at https://vimeo.com/96289935
+
+--------------------------------------------------------------------
+
+Added PWM functionality
--- ledccelerometer.py
+++ ledccelerometer.py
@@ -1,31 +1,49 @@
 #!/usr/bin/env python
 # coding: latin-1
-# script by Stevica Kuharski, @kstevica
+# coded by Stevica Kuharski, @kstevica
 
 import XLoBorg
 import time
 import RPi.GPIO as GPIO
 import math
 
+# pins that you'll use
+use_pin_x = 11
+use_pin_y = 13
+use_pin_z = 15
+
+# setup
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
-GPIO.setup(13, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
-GPIO.output(11, False)
-GPIO.output(13, False)
-GPIO.output(15, False)
+GPIO.setup(use_pin_x, GPIO.OUT)
+GPIO.setup(use_pin_y, GPIO.OUT)
+GPIO.setup(use_pin_z, GPIO.OUT)
+GPIO.output(use_pin_x, False)
+GPIO.output(use_pin_y, False)
+GPIO.output(use_pin_z, False)
 
+# XLoBorg initialization
 XLoBorg.printFunction = XLoBorg.NoPrint
-
 XLoBorg.Init()
 
+# init globals
 old_x, old_y, old_z = XLoBorg.ReadAccelerometer()
-
 use_sensitivity = 1
 
-def light(pin, on_off):
-	GPIO.output( pin, on_off)
+# setup software PWM
+light_x = GPIO.PWM(use_pin_x, 100)
+light_y = GPIO.PWM(use_pin_y, 100)
+light_z = GPIO.PWM(use_pin_z, 100)
 
+# turn light on
+def light(pin, on_off, strength):
+	if (on_off==True):
+		strength = min(strength*200, 100)
+		pin.start(strength)
+	else:
+		pin.stop()
+
+# main loop
 while True:
 	should_x = False
 	should_y = False
@@ -40,13 +58,11 @@ while True:
 		should_x = True
 	if (y!=old_y):
 		should_y = True
-	old_x = x
-	old_y = y
-	old_z = z
-	light(11, should_x)
-	light(13, should_y)
-	light(15, should_z)
+	light(light_x, should_x, abs(old_x-x))
+	light(light_y, should_y, abs(old_y-y))
+	light(light_z, should_z, abs(old_z-z))
 	if (should_x or should_y or should_z):
-		time.sleep(1)
-
-	time.sleep(0.1)
+		time.sleep(0.5)
+        old_x = x
+        old_y = y
+        old_z = z
